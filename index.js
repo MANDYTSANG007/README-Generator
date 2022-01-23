a17b46959caded5861bafef1fb8f95db64833b05
Mandy Tsang <mandy.tsang007@gmail.com>
2022-01-22 22:19:56 -0800

    add npm, package.json, and inquirer

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 //TODO: Create an array of questions for user input
+const questions = [];
 //TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
 //TODO: Create a function to initialize app
+function init() {}
 //Function call to initalize app
-
+init();
 
 
 
