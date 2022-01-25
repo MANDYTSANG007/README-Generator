412e17ba4df2ef6259e6b2d6caed00f70fb3ab59
Mandy Tsang <mandy.tsang007@gmail.com>
2022-01-24 22:35:41 -0800

    adjust indentation

--- index.js
+++ index.js
@@ -12,51 +12,51 @@ const questions = [
         name: "username"
     },
     {
-            type: "input",
-            message: "What is your email address?",
-            name: "email"
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
     },
     {
-            type: "input",
-            message: "What is your project's title?",
-            name: "title"
+        type: "input",
+        message: "What is your project's title?",
+        name: "title"
     },
-        {
-            type: "input",
-            message: "Please write your project's description.",
-            name: "description"
-        },
-        {
-            type: "input",
-            message: "What are your project's installation instructions?",
-            name: "installation"
-        },
-        {
-            type: "input",
-            message: "What is your project's usage information?",
-            name: "usage"
-        },
-        {
-            type: "list",
-            message: "What license should your project include?",
-            name: "license",
-            choices: ["MIT", "GPLv2", "Apache", "BSD 3-Clause", "none"]
-        },
-        {
-            type: "input",
-            message: "Who are the contributors?",
-            name: "contributors"
-        },
-        {
-            type: "input",
-            message: "What is your project's test instructions?",
-            name: "test"
-        },
-        {
-            type: "input",
-            message: "Any questions about this repository?",
-            name: "questions"
-        },
+    {
+        type: "input",
+        message: "Please write your project's description.",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "What are your project's installation instructions?",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "What is your project's usage information?",
+        name: "usage"
+    },
+    {
+        type: "list",
+        message: "What license should your project include?",
+        name: "license",
+        choices: ["MIT", "GPLv2", "Apache", "BSD 3-Clause", "none"]
+    },
+    {
+        type: "input",
+        message: "Who are the contributors?",
+        name: "contributors"
+    },
+    {
+        type: "input",
+        message: "What is your project's test instructions?",
+        name: "test"
+    },
+    {
+        type: "input",
+        message: "Any questions about this repository?",
+        name: "questions"
+    },
 ];
 
 //function to write README file
@@ -100,29 +100,6 @@ init();
 
 
 
-
-
-
-
-
-
-
-// 
-// GIVEN a command-line application that accepts user input
-//WHEN I am prompted for information about my application repository
-// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-//WHEN I enter my project title
-//THEN this is displayed as the title of the README
-// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-//THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-// WHEN I choose a license for my application from a list of options
-//THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-// WHEN I enter my GitHub username
-//THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-// WHEN I enter my email address
-//THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-// WHEN I click on the links in the Table of Contents
-//THEN I am taken to the corresponding section of the README
 
 
 
